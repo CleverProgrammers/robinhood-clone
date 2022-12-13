0a4f9d201bcfa02a843bfb9696ff65f87697bf7a
areyou4real <119920294+areyou4real@users.noreply.github.com>
2022-12-13 04:49:21 +0000

    Pending changes exported from your codespace

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
 import "./Header.css";
-import Logo from './robinhood.svg'
+import Logo from './stocki.svg'
 import Signup from './signup.js'
 
 function Header() {
   return (
     <div className="header__wrapper">
       <div className="header__logo">
-        <img src={Logo} width={25}/>
+        <img src={Logo} width={100}/>
       </div>
       <div className="header__search">
         <div className="header__searchContainer">
@@ -17,9 +17,9 @@ function Header() {
         </div>
       </div>
       <div className="header__menuItems">
-        <a href="/">Free Stocks</a>
-        <a href="/">PortFolio</a>
-        <a href="/bank.html">Cash</a>
+        <a href="/">Portfolio</a>
+        <a href="/market.html">Market</a>
+        <a href="/bank.html">Bank</a>
         <a href="/">Sign Up</a>
       </div>
     </div>
--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import firebase from "firebase";
 
 const key = "bv1uf4v48v6o5ed6h88g";
 
